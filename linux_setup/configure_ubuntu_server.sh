8e7c2e9f198742fe0b7be72df401399468d6b3b1
= <=>
2024-04-11 14:35:56 +0200

    Update Python version to 3.11.8 and add opdracht_2.py dag

--- linux_setup/configure_ubuntu_server.sh
+++ linux_setup/configure_ubuntu_server.sh
@@ -1,73 +1,60 @@
+#!/bin/bash
 
-sudo su
+set -e
 
 # Add docker repo
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Add vs-code repo
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 
 # Add pgadmin repo
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" | sudo tee /etc/apt/sources.list.d/pgadmin4.list > /dev/null
 
 # Install all packages
-apt-get update
-apt-get upgrade -y
-apt-get install -y wget gpg curl gnupg lsb-release xrdp gh apt-transport-https docker-ce docker-ce-cli containerd.io docker-compose-plugin pgadmin4-desktop
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y wget gpg curl gnupg lsb-release xrdp gh apt-transport-https docker-ce docker-ce-cli containerd.io docker-compose-plugin pgadmin4-desktop code
 
-# Install xfce4 seperately
-DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
-apt-get install -y xfce4-session
+# Install xfce4 separately
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+sudo apt-get install -y xfce4-session
 
 # Install asdf and python
-apt install -y build-essential libssl-dev zlib1g-dev \
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
-echo "
-# asdf initialisation
-if [ -f $HOME/.asdf/asdf.sh ]; then
-    . $HOME/.asdf/asdf.sh
-fi
-
-# asdf completions initialisation
-if [ -f $HOME/.asdf/completions/asdf.bash ]; then
-    . $HOME/.asdf/completions/asdf.bash
-fi" >> /home/cursist1/.bashrc 
-bash && asdf plugin-add python
-asdf install python 3.10.9
-asdf global python 3.10.9
+echo -e "\n# asdf initialisation\nif [ -f $HOME/.asdf/asdf.sh ]; then\n    . $HOME/.asdf/asdf.sh\nfi\n\n# asdf completions initialisation\nif [ -f $HOME/.asdf/completions/asdf.bash ]; then\n    . $HOME/.asdf/completions/asdf.bash\nfi" >> ~/.bashrc 
+source ~/.bashrc
+asdf plugin-add python
+asdf install python 3.11.8
+asdf global python 3.11.8
 
 # Setup xrdp
-systemctl enable xrdp;
-adduser xrdp ssl-cert;
-echo xfce4-session >~/.xsession;
-service xrdp restart;
+sudo systemctl enable xrdp
+sudo adduser xrdp ssl-cert
+echo xfce4-session >~/.xsession
+sudo service xrdp restart
 
 # Install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt --fix-broken install
+sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt --fix-broken install -y
 rm -f google-chrome-stable_current_amd64.deb
 
 # Remove usage of sudo when using docker
-usermod -aG docker cursist1
+sudo usermod -aG docker $USER
 
-# Change to cursist1 user
-su - cursist1
-
-# Pull devsetup
-echo '' > my_token.txt
-gh auth login --with-token < my_token.txt
-gh repo clone DatalabFabriek/dl-air-devsetup /home/cursist1/airflow-development
+# Clone the development setup
+gh auth login
+gh repo clone DatalabFabriek/dl-air-devsetup /home/$USER/airflow-development
 gh auth logout
-rm -rf /home/cursist1/airflow-development/.git
-rm -f my_token.txt
+rm -rf ~/airflow-development/.git
--- linux_setup/create_vm.sh
+++ linux_setup/create_vm.sh
@@ -1,16 +1,47 @@
-az vm create \
-    --resource-group KoensSpeelzaal \
-    --name airflowcursus-vm4 \
-    --image Canonical:0001-com-ubuntu-server-jammy:22_04-lts:22.04.202301140 \
-    --authentication-type password \
-    --admin-username cursist1 \
-    # --admin-password  \
-    --os-disk-delete-option Delete \
-    --size Standard_D2ds_v5 \
-    --nic-delete-option Delete \
-    --public-ip-sku Standard \
-    --nsg-rule SSH
-    # --custom-data cloud-init.txt
+#!/bin/bash
 
-# Sidestep
-az vm open-port --resource-group KoensSpeelzaal --name airflowcursus-vm4 --port 3389
+# Check if at least two arguments were provided
+if [ $# -lt 2 ]
+then
+    echo "Please provide at least one number and password pair as arguments."
+    exit 1
+fi
+
+# Convert the arguments into an array
+args=("$@")
+
+# Loop over the arguments two by two
+for ((i=0; i<$#; i+=2))
+do
+    # Get the number and password
+    number=${args[$i]}
+    password=${args[$((i+1))]}
+    
+    echo "Creating VM with number: $number"
+
+    # Create the VM and capture the output
+    output=$(az vm create \
+        --resource-group KoensSpeelzaal \
+        --name airflowcursus-vm$number \
+        --image Canonical:0001-com-ubuntu-server-jammy:22_04-lts:22.04.202301140 \
+        --authentication-type password \
+        --admin-username cursist$number \
+        --admin-password $password \
+        --os-disk-delete-option Delete \
+        --size Standard_D2ds_v5 \
+        --nic-delete-option Delete \
+        --public-ip-sku Standard \
+        --nsg-rule SSH)
+
+    echo "VM airflowcursus-vm$number created."
+
+    # Extract the public IP address and print it
+    publicIpAddress=$(echo $output | jq -r '.publicIpAddress')
+    echo "Public IP Address for VM airflowcursus-vm$number is: $publicIpAddress"
+
+    # Open the port
+    az vm open-port --resource-group KoensSpeelzaal --name airflowcursus-vm$number --port 3389 > /dev/null
+    echo "Port 3389 opened for VM airflowcursus-vm$number."
+done
+
+echo "Script execution completed."
